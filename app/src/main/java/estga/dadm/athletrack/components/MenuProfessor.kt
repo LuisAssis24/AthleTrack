ca1b9fa1f4a5587f76805b1c527d1f8efb084315
NO0BMaster69 <luishassis@ua.pt>
2025-04-10 12:12:06 +0100

    Divisão dos Menus

--- app/src/main/java/estga/dadm/athletrack/components/MenuProfessor.kt
+++ app/src/main/java/estga/dadm/athletrack/components/MenuProfessor.kt
@@ -1,7 +1,62 @@
 package estga.dadm.athletrack.components
 
-fun MenuProfessor() {
-    // Implementação do menu lateral para o professor
-    // Aqui você pode adicionar os itens do menu, como "Perfil", "Treinos", "Configurações", etc.
-    // Utilize componentes como Column, Row, Text, Icon, etc. para criar a interface desejada.
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import estga.dadm.athletrack.ui.theme.GrayNeutral
+import estga.dadm.athletrack.ui.theme.White
+
+@Composable
+fun MenuProfessor(userName: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(0.7f)
+            .fillMaxHeight()
+            .background(DarkGray)
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .background(GrayNeutral),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "Foto de Perfil",
+                tint = White,
+                modifier = Modifier.size(64.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = userName,
+            fontSize = 18.sp,
+            color = White
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Opções específicas do professor
+        Text("Avaliações", color = White)
+        Text("Gestão de Treinos", color = White)
+        Text("Listar Atletas", color = White)
+    }
 }
